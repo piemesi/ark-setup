04b1675d5bcdcabeb60a10183b0dacfb60204c8d
Alexey Gaynulin <gaynulin@google.com>
2022-09-02 10:58:11 +0000

    initial setup commit

---
+++ app/init/init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Collect static files
+#echo "Collect static files"
+#python manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "Apply database migrations"
+python /code/manage.py migrate
+
+# Start server
+#echo "Starting server"
+#python manage.py runserver 0.0.0.0:8000
+
+#tail -f /dev/null
+
+cd /code && gunicorn admin.wsgi -b :8000 --env DJANGO_SETTINGS_MODULE=admin.debug_settings --threads 4 --timeout 300
+
